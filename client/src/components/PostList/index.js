023ceabe3fd5e39930b6920b5c551df2f296f1f4
Andrewy2416 <andrewy2416@gmail.com>
2022-03-01 15:26:56 -0800

    added post form and header

---
+++ client/src/components/PostList/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PostList = ({ posts, title }) => {
+  if (!posts.length) {
+    return <h3>No Posts Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {posts &&
+        posts.map(post => (
+          <div key={post._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`/profile/${post.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {post.username}
+              </Link>{' '}
+              post on {post.createdAt}
+            </p>
+            <div className="card-body">
+              <Link to={`/post/${post._id}`}>
+                <p>{post.postText}</p>
+                <p className="mb-0">
+                  Reactions: {post.reactionCount} || Click to{' '}
+                  {post.reactionCount ? 'see' : 'start'} the discussion!
+                </p>
+              </Link>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ThoughtList;
