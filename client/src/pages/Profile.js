a2b2314e95683759182e3e4397a45b4c8f9e1361
Andrewy2416 <andrewy2416@gmail.com>
2022-03-01 15:49:10 -0800

    updated redirect

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 
 import PostForm from '../components/PostForm';
 import PostList from '../components/PostList';
@@ -22,7 +22,7 @@ const Profile = (props) => {
 
   // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Redirect to="/profile" />;
+    return <Navigate to="/profile" />;
   }
 
   if (loading) {
