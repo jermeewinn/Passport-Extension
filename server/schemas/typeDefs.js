99d5f12c1b22263f8b6a8c87cbeb580ef191dad5
Andrewy2416 <andrewy2416@gmail.com>
2022-03-01 13:54:05 -0800

    added schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    posts: [Post]
+    friends: [User]
+  }
+
+  type Post {
+    _id: ID
+    postText: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comments]
+  }
+
+  type Comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addComment(postId: ID!, commentBody: String!): Post
+    addFriend(friendId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
