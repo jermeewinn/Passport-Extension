92c468ebc9042e72f04a2d62f38dc67607f19592
Jeremy Huynh <jeahuynh18@gmail.com>
2022-03-01 13:58:52 -0800

    created Post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,9 +1,37 @@
 const { Schema, model } = require('mongoose');
 const commentSchema = require('./Comment');
-const date = require('../utils/dateFormat');
+const dateFormat = require('../utils/dateFormat');
 
 const postSchema = new Schema(
     {
-        
+        postText: {
+            type: String,
+            required: 'You nned to type something to submit a post!',
+            minlengh: 1,
+            maxlength: 300
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        comments: [commentSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
     }
-)
+);
+
+postSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
